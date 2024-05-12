f3ac36cc7e618b7548d0445f4f631b144166c94e
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-13 00:49:09 +0200

    feat: prevent form reload at submit

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 'use strict'
+
+const formMain = document.getElementById('main-form');
+console.log(formMain);
+
+formMain.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    console.log("Ho inviato il form");
+
+}
+)
