2e75fc95d452563fc5b370a44cd5ff4b3dc1e613
marco-carangelo <marcocarangelo2016@gmail.com>
2024-05-13 14:32:10 +0200

    feat: result printer

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # js-biglietto-treno-form
+
+Il programma dovrà mostrare una form da compilare usando i corretti campi HTML di input con cui chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero. Utilizzate Bootstrap come libreria UI.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+il prezzo del biglietto è definito in base ai km (0.21 € al km)
+va applicato uno sconto del 20% per i minorenni
+va applicato uno sconto del 40% per gli over 65.
+Il recap dei dati e l'output del prezzo finale va stampato in pagina (formattato con massimo due decimali, per indicare centesimi sul prezzo).
+
+Creazione interfaccia grafica:
+
+-La pagina HTML avrà due parti, un'intestazione con il titolo dell'esercizio ed una sezione principale, che a sua volta sara divisa in due parti, una conterrà il form per l'inserimento dei dati da parte dell'utente e l'altra conterrà un campo che visualizzerà il risultato.
+-Il form conterrà due campi, uno per inserire l'età e l'altro per la distranza da percorrere. Per l'età si potrebbe usare un campo range, in modo da importare facilmente un'età minima (1) ed un'età massima (122 che è l'età più alta mai registrata). Per la distanza si potrebbe usare un normale campo numerico e si potrebbe validare il dato successivamente per evitare valori negativi.
+-Per il risultato si userà un campo di testo  
--- js/main.js
+++ js/main.js
@@ -1,16 +1,5 @@
 'use strict'
 
-// Prevent page reload at form submit
-const formMain = document.getElementById('main-form');
-console.log(formMain);
-
-formMain.addEventListener('submit', function (event) {
-    event.preventDefault();
-
-    console.log("Ho inviato il form");
-
-}
-)
 
 // Age label
 
@@ -22,3 +11,61 @@ yearsLabel.innerText = userAge.value;
 userAge.addEventListener('change', function (event) {
     yearsLabel.innerText = userAge.value;
 })
+
+// Prevent page reload at form submit and main function
+const formMain = document.getElementById('main-form');
+console.log(formMain);
+
+formMain.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    //Variables definition
+    const userDistance = document.getElementById('user-distance');
+    const userAge = document.getElementById('user-age');
+    const youngDiscount = 20;
+    const seniorDiscount = 40;
+    const kmPrice = 0.21;
+
+    let price = 0;
+    let discountPrint;
+
+    //fullprice calculation
+
+    price = userDistance.value * kmPrice;
+    console.log(price);
+
+    //Discount verification
+
+    if (userAge.value < 18) {
+
+        price -= (price * youngDiscount) / 100;
+        console.log("L'utente ha diritto ad uno sconto del 20%");
+        discountPrint = "L'utente ha diritto ad uno sconto del 20%."
+
+    } else {
+        if (userAge.value > 65) {
+
+            price -= (price * seniorDiscount) / 100;
+            console.log("L'utente ha diritto ad uno sconto del 40%");
+            discountPrint = "L'utente ha diritto ad uno sconto del 40%."
+
+
+        } else {
+
+            console.log("L'utente non ha diritto a sconti");
+            discountPrint = "L'utente non ha diritto a sconti."
+
+
+        }
+    }
+
+    //Print result
+    document.getElementById('discount-print').innerText = discountPrint;
+    document.getElementById('result').innerHTML = price.toFixed(2) + '€';
+
+
+
+
+    console.log(price);
+});
+
